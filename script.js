c71e25748877611a0974f84b2e56ab85ada8c4c7
sid6145 <sidtdesh@gmail.com>
2021-04-06 12:00:49 +0530

    object prototype practice

---
+++ script.js
@@ -0,0 +1,11 @@
+function Name(firstname, lastname){
+    this.first = firstname;
+    this.last = lastname;
+}
+
+Name.prototype.fullName = function(){
+    return this.first + " " + this.last;
+}
+let person1 = new Name("Siddhant", "Deshmukh")
+
+console.log(person1.fullName());
